4a81150c3b88dc419bb066b5b41db19b4cf20b5a
Stefan Haller <stefan.haller@stha.de>
2025-10-28 08:56:07 +0100

    fix: Improve error reporting

--- rm-mdns.c
+++ rm-mdns.c
@@ -31,10 +31,16 @@ static int obtain_ipv4_address(struct sockaddr_in *addr) {
   strncpy(ifr.ifr_name, RM_MDNS_WIFI_IFACE, IFNAMSIZ - 1);
 
   int fd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (ioctl(fd, SIOCGIFADDR, &ifr) == -1) {
-    perror("ioctl");
+  if (fd == -1) {
+    perror("[obtain_ipv4_address] Could not open socket");
     return -1;
   }
+
+  if (ioctl(fd, SIOCGIFADDR, &ifr) == -1) {
+    perror("[obtain_ipv4_address] Could not get interface address");
+    return -1;
+  }
+
   close(fd);
 
   memcpy(addr, &ifr.ifr_addr, sizeof(struct sockaddr_in));
@@ -103,7 +109,7 @@ static int open_listening_socket() {
 
   int socket = mdns_socket_open_ipv4(&sock_addr);
   if (socket == -1) {
-    perror("socket");
+    perror("[open_listening_socket] Could not open socket");
     exit(EXIT_FAILURE);
   }
 
@@ -121,14 +127,14 @@ static int setup_signal_handler() {
   // Block signals so that they are not handled by default handler.
   // Otherwise they would not trigger the signalfd.
   if (sigprocmask(SIG_BLOCK, &mask, nullptr) == -1) {
-    perror("sigprocmask");
+    perror("[setup_signal_handler] Could not mask signals");
     exit(EXIT_FAILURE);
   }
 
   // Create new signalfd.
   int sfd = signalfd(-1, &mask, 0);
   if (sfd == -1) {
-    perror("signalfd");
+    perror("[setup_signal_handler] Could not create signalfd");
     exit(EXIT_FAILURE);
   }
 
@@ -141,7 +147,7 @@ static int setup_epoll(int socketfd, int sigfd) {
   // Create epoll instance.
   int epollfd = epoll_create1(0);
   if (epollfd == -1) {
-    perror("epoll_create1");
+    perror("[setup_epoll] Could not create epollfd");
     exit(EXIT_FAILURE);
   }
 
@@ -149,7 +155,7 @@ static int setup_epoll(int socketfd, int sigfd) {
   ev.events = EPOLLIN;
   ev.data.fd = socketfd;
   if (epoll_ctl(epollfd, EPOLL_CTL_ADD, socketfd, &ev) == -1) {
-    perror("epoll_ctl");
+    perror("[setup_epoll] Could not add socket to epollfd");
     exit(EXIT_FAILURE);
   }
 
@@ -157,7 +163,7 @@ static int setup_epoll(int socketfd, int sigfd) {
   ev.events = EPOLLIN;
   ev.data.fd = sigfd;
   if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sigfd, &ev) == -1) {
-    perror("epoll_ctl");
+    perror("[setup_epoll] Could not add signalfd to epollfd");
     exit(EXIT_FAILURE);
   }
 
@@ -172,7 +178,7 @@ int main(int argc, const char *const *argv) {
   for (;;) {
     struct epoll_event ev;
     if (epoll_wait(epollfd, &ev, 1, -1) == -1) {
-      perror("epoll_wait");
+      perror("[main] Failure on epoll_wait");
       exit(EXIT_FAILURE);
     }
 
